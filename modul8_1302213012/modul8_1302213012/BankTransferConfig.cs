be0fe4ee4c9bbf46f4c9e240cffe37f99a3c09fe
Bimo Zachriansyah Wicaksono Hermawan <bimozachri@gmail.com>
2023-04-12 16:48:34 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ modul8_1302213012/modul8_1302213012/BankTransferConfig.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_1302213012
+{
+    public class BankTransferConfig
+    {
+        public BankTransfer BT;
+        private const string loc = ".";
+        private const string path = loc + "\\" + @"bank_transfer_config.json";
+        public transfer TF;
+        public confirmation confirm;
+
+        public BankTransferConfig()
+        {
+            try
+            {
+                readConfig();
+            }
+            catch
+            {
+                setDefault();
+                writeBankTransferConfig();
+            }
+        }
+
+        public void setDefault()
+        {
+            BT = new BankTransfer("en", new transfer(25000000, 6500, 15000),
+                new List<string> { "RTO (real-time)", "SKN", "RTGS", "BI FAST" },
+                new confirmation("yes", "ya"));
+        }
+
+        public void writeBankTransferConfig()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+            };
+
+            string json = JsonSerializer.Serialize(BT, options);
+            File.WriteAllText(path, json);
+        }
+
+        public void readConfig()
+        {
+            string read = File.ReadAllText(path);
+            BT = JsonSerializer.Deserialize<BankTransfer>(read);
+        }
+    }
+    public class BankTransfer
+    {
+        public string lang { get; set; }
+        public transfer transfer { get; set; }
+        public List<string> methods { get; set; }
+        public confirmation confirmation { get; set; }
+
+        public BankTransfer() { }
+        public BankTransfer(string lang, transfer transfer, List<string> methods, confirmation confirmation)
+        {
+            this.lang = lang;
+            this.transfer = transfer;
+            this.methods = methods;
+            this.confirmation = confirmation;
+        }
+    }
+
+    public class transfer
+    {
+        public int threshold { get; set;}
+        public int low_fee { get; set;}
+        public int high_fee { get; set;}
+
+        public transfer(int threshold, int low_fee, int high_fee)
+        {
+            this.threshold = threshold;
+            this.low_fee = low_fee;
+            this.high_fee = high_fee;
+        }
+
+        public transfer() { }
+    }
+
+    public class confirmation
+    {
+        public string en { get; set; }
+        public string id { get; set; }
+
+        public confirmation(string en, string id)
+        {
+            this.en = en;
+            this.id = id;
+        }
+
+        public confirmation() { }
+    }
+}
--- modul8_1302213012/modul8_1302213012/Program.cs
+++ modul8_1302213012/modul8_1302213012/Program.cs
@@ -1,2 +1,91 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul8_1302213012;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BankTransferConfig config = new BankTransferConfig();
+
+        if (config.BT.lang.Contains("en"))
+        {
+            Console.WriteLine("Please insert the amount of money to transfer:");
+        }
+        else if (config.BT.lang.Contains("id"))
+        {
+            Console.WriteLine("Masukkan jumlah uang yang akan di-transfer");
+        }
+
+        string transfer = Console.ReadLine();
+        int jumlahTransfer = (int)Convert.ToInt32(transfer);
+
+        int biayaTransfer = 0;
+        if (jumlahTransfer <= config.BT.transfer.threshold)
+        {
+            biayaTransfer = config.BT.transfer.low_fee;
+        }
+        else if (jumlahTransfer > config.BT.transfer.threshold)
+        {
+            biayaTransfer = config.BT.transfer.high_fee;
+        }
+
+        if (config.BT.lang.Contains("en"))
+        {
+            Console.WriteLine("Transfer fee = " + biayaTransfer + "\nTotal amount = " + (jumlahTransfer + biayaTransfer));
+        }
+        else if (config.BT.lang.Contains("id"))
+        {
+            Console.WriteLine("Biaya Transfer = " + biayaTransfer + "\nTotal biaya = " + (jumlahTransfer + biayaTransfer));
+        }
+
+        if (config.BT.lang.Contains("en"))
+        {
+            Console.WriteLine("Select transfer method : ");
+        }
+        else if (config.BT.lang.Contains("id"))
+        {
+            Console.WriteLine("Pilih metode transfer : ");
+        }
+
+        for (int i = 0; i < config.BT.methods.Count; i++)
+        {
+            Console.WriteLine((i + 1) + ". " + config.BT.methods[i]);
+        }
+
+        Console.Write("Pilih: ");
+        string metode = Console.ReadLine();
+        int metodeTransfer = (int)Convert.ToInt32(transfer);
+
+        if (config.BT.lang.Contains("en"))
+        {
+            Console.WriteLine("Please type " + config.BT.confirmation.en + " to confirm the transaction:");
+        }
+        else if (config.BT.lang.Contains("id"))
+        {
+            Console.WriteLine("Ketik " + config.BT.confirmation.id + " untuk mengkonfirmasi transaksi:");
+        }
+
+        string inputKonfirmasi = Console.ReadLine();
+
+        if (config.BT.lang.Contains("en"))
+        {
+            if (inputKonfirmasi.Contains(config.BT.confirmation.en))
+            {
+                Console.WriteLine("The transfer is completed");
+            }
+            else
+            {
+                Console.WriteLine("Transfer is cancelled");
+            }
+        }
+        else if (config.BT.lang.Contains("id"))
+        {
+            if (inputKonfirmasi.Contains(config.BT.confirmation.id))
+            {
+                Console.WriteLine("Proses transfer berhasil");
+            }
+            else
+            {
+                Console.WriteLine("Transfer dibatalkan");
+            }
+        }
+    }
+}
